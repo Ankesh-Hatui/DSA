42612a935acbca83105aa908fe2eb28c3ab1f4d9
Ankesh-Hatui <155291507+Ankesh-Hatui@users.noreply.github.com>
2024-11-09 21:29:48 +0530

    Create  Minimum Array End.cpp

---
+++ Bit-Maniplulation/Minimum Array End.cpp
@@ -0,0 +1,18 @@
+/*
+LeetCode : 3133
+T.C : O(N)
+S.C : O(1)
+*/
+
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long num=x;
+
+        for (int i=1;i<n;i++){
+            num=(num+1)|x;
+
+        }
+        return num;
+    }
+};
