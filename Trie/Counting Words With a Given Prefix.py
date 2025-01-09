b1dfa656c75f1c670fc9fdf8b2de4ae1cb139309
Ankesh-Hatui <155291507+Ankesh-Hatui@users.noreply.github.com>
2025-01-09 20:09:02 +0530

    Update Counting Words With a Given Prefix.py

--- Trie/Counting Words With a Given Prefix.py
+++ Trie/Counting Words With a Given Prefix.py
@@ -13,3 +13,57 @@ class Solution:
             if pref_str==pref:
                 cnt+=1
         return cnt
+----------------------------------------------------------------------------------------------------------------------------------------------------
+'''
+* T.C: O(N*L) where L is length of the word and N is the no of words
+* S.C : O(N*L) for every N words stored and L is the length of that word
+* using Tries Solution
+'''
+class Tree{
+    public:
+    Tree *child[26];
+    bool end=false;
+    int cnt=0;
+    Tree(){
+        this->end=false;
+        for (int i=0;i<26;i++){
+            this->child[i]=nullptr;
+        }
+        this->cnt=0;
+    }
+};
+void insert(Tree*root,string s){
+    Tree *curr=root;
+    for (auto&ch:s){
+        if (curr->child[ch-'a']==nullptr){
+            curr->child[ch-'a']=new Tree();
+        }
+        
+        curr=curr->child[ch-'a'];
+        curr->cnt+=1;
+    }
+    curr->end=true;
+}
+int search(Tree*root,string s){
+    Tree *curr=root;
+    int count=0;
+    for (auto&ch:s){
+        if (curr->child[ch-'a']==nullptr){
+            return 0;
+        }
+        
+        curr=curr->child[ch-'a'];
+        
+    }
+    return curr->cnt;
+}
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        Tree*root=new Tree();
+        for (auto&str:words){
+            insert(root,str);
+        }
+        return search(root,pref);
+    }
+};
