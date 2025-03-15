1bfa1a0d13e0671e025ba375034152545ba75cfc
Ankesh-Hatui <155291507+Ankesh-Hatui@users.noreply.github.com>
2025-03-15 14:45:35 +0530

    Update House Robber IV.cpp

--- Binary Search/House Robber IV.cpp
+++ Binary Search/House Robber IV.cpp
@@ -34,3 +34,51 @@ public:
         return ans;
     }
 };
+
+
+
+
+
+// Optimal Solution
+
+// Binary Search
+// T.C : O(NlogM) , where M is maximum possible capability
+// S.C : O(1)
+
+class Solution {
+    bool isValid(vector<int>&nums,int num,int k){
+        int cnt = 0;
+        for (int i=0;i<nums.size();){
+            if (nums[i]<=num){
+                cnt += 1;
+                i+=2;
+            }
+            else{
+                i+=1;
+            }
+        }
+        if (cnt>=k){
+            return true;
+        }
+        return false;
+    }
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int left = *min_element(nums.begin(),nums.end());
+        int right = *max_element(nums.begin(),nums.end());
+
+        int ans = 0;
+
+        while (left<=right){
+            int mid = left + (right-left)/2;
+            if (isValid(nums,mid,k)){
+                ans = mid;
+                right = mid-1;
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return ans ;
+    }
+};
